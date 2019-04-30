d869f35fa5785fafed4d9c1bfd5057abd4a89b89
Mark Entingh <entingh@gmail.com>
2019-04-29 20:59:12 -0700

    updated README and copied working plugin files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Saber-CORS
-A vendor plugin for Saber that handles cross-origin requests
+A vendor plugin for [Saber](https://github.com/Datasilk/Saber) that handles cross-origin requests.
+
+### Prerequisites
+* Visual Studio 2017
+* Clone [Saber](https://github.com/Datasilk/Saber) repository
+
+### Installation
+* Clone this repository inside your Saber project within `/App/Vendor/` and name the folder **CORS**
+* Copy `/App/Vendor/CORS/config.template.json` to `/App/Vendor/CORS/config.json` and open the file for modification
+	* Add at least one URL to the `origins:development` array, e.g. `"https://localhost:5000"`
+    * Add at least one URL to the `origins:production` array, e.g. `"https://www.mycoolsite.io"`
+
+#### Usage
+When a 3rd-party website makes Web API calls to your website on a public domain other than your own domain, this plugin will authorize access via **CORS** as long as you include the 3rd-party domain within the *config.json* file.
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+public partial class Startup : Datasilk.Startup
+{
+
+    partial void ConfigureVendorServices(IServiceCollection services)
+    {
+        services.AddCors();
+    }
+
+    partial void ConfigureVendors(IApplicationBuilder app)
+    {
+        ConfigureCORS(app);
+    }
+
+    private void ConfigureCORS(IApplicationBuilder app)
+    {
+        //use CORS for cross-domain requests
+        var config = new ConfigurationBuilder()
+                .AddJsonFile(Server.MapPath("/Vendor/CORS/config.json"))
+                .AddEnvironmentVariables().Build();
+
+        var origins = new string[] { };
+
+        switch (Server.environment)
+        {
+            case Server.Environment.development:
+                origins = config.GetSection("origins:development").Get<string[]>();
+                break;
+            case Server.Environment.production:
+                origins = config.GetSection("origins:production").Get<string[]>();
+                break;
+            case Server.Environment.staging:
+                origins = config.GetSection("origins:staging").Get<string[]>();
+                break;
+        }
+
+        app.UseCors(builder =>
+        {
+            builder.WithOrigins(
+                "chrome-extension://" +
+                (
+                    Server.environment == Server.Environment.development ?
+                    "kdcpigikfhpokfbbklgdeeheajkndiam" :  //development
+                    "" //production
+                )
+            )
+            .WithHeaders("GET", "POST", "OPTIONS")
+            .WithHeaders("*")
+            .AllowCredentials();
+        });
+    }
+}
