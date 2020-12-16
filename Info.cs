6bccb41b074bef41c51fcb5ea7ba83a4a468a548
Mark Entingh <entingh@gmail.com>
2020-12-15 22:33:55 -0800

    added info class

---
+++ Info.cs
@@ -0,0 +1,12 @@
+﻿using Saber.Vendor;
+
+namespace Saber.Vendors.CORS
+{
+    public class Info : IVendorInfo
+    {
+        public string Key { get; set; } = "CORS";
+        public string Name { get; set; } = "CORS";
+        public string Description { get; set; } = "Enables CORS (Cross-Origin Resource Sharing) headers in Saber";
+        public Version Version { get; set; } = "1.0.0.0";
+    }
+}
